7bf18c8f099ab63c6ce4b96e0c4de1b785f7147a
Juul van Orsouw <juulvorsouw@ziggo.nl>
2024-09-26 20:04:55 +0200

    Adding tailwind version of campfire

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#000000',
+        'white': '#FFFFFF',
+        'dark-background': '#420c0c',
+        'darker-background': '#202632',
+        'text-color-primary': '#FFFFFF',
+        'text-color-secondary': '#b0b0b0',
+        'text-color-sidebar': '#b0b0b0',
+        'accent-red': '#ec0909',
+        'accent-gray': '#5A5B4D',
+        'shadow-light': 'rgba(0, 0, 0, 0.4)',
+        'background': '#000000',
+      },
+    },
   },
   plugins: [],
 }
-
